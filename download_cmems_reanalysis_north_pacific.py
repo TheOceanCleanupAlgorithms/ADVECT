a6ed843b856ef1c2fde0f71ab29a6d75a271d18e
dklink <dougdklink3@gmail.com>
2020-09-10 10:36:31 -0600

    moved password into .env

---
+++ download_cmems_reanalysis_north_pacific.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from datetime import timedelta as td
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+dates = pd.date_range('2000-01-01', '2000-1-31', freq='1D')
+
+for date in dates:
+    filename = f"uv_{date.strftime('%Y-%m-%d')}.nc"
+    print(f'Downloading {filename}')
+
+    cmd = f"""python -m motuclient --motu http://my.cmems-du.eu/motu-web/Motu \
+    --service-id GLOBAL_REANALYSIS_PHY_001_030-TDS --product-id global-reanalysis-phy-001-030-daily \
+    --longitude-min 100 --longitude-max 280 --latitude-min 0 --latitude-max 70 \
+    --date-min "{date}" --date-max "{date+td(days=1)}" \
+    --depth-min 0.493 --depth-max 5727.918 \
+    --variable uo --variable vo \
+    --out-dir ./ --out-name cmems/{filename} \
+    --user {os.getenv('copernicus_uname')} --pwd '{os.getenv('copernicus_pwd')}'"""
+
+    os.system(cmd)
