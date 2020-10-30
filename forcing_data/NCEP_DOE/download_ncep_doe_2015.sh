e47813369f25ae52e2c5c17d4e65b09d14756331
dklink <dougdklink3@gmail.com>
2020-10-29 16:09:23 -1000

    data downloading

---
+++ forcing_data/NCEP_DOE/download_ncep_doe_2015.sh
@@ -0,0 +1,3 @@
+# nice and easy... for testin'
+wget ftp://ftp2.psl.noaa.gov/Datasets/ncep.reanalysis2.dailyavgs/gaussian_grid/uwnd.10m.gauss.2015.nc ./
+wget ftp://ftp2.psl.noaa.gov/Datasets/ncep.reanalysis2.dailyavgs/gaussian_grid/vwnd.10m.gauss.2015.nc ./
