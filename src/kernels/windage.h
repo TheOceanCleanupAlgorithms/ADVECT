2074c442b5ac634f7dd1ba67292bf105d9323c2d
dklink <dougdklink3@gmail.com>
2020-11-13 16:37:39 -1000

    made windage conditional on depth

--- src/kernels/windage.h
+++ src/kernels/windage.h
@@ -4,6 +4,8 @@
 #include "fields.h"
 #include "particle.h"
 
+#define MINIMUM_WINDAGE_DEPTH 1  // meters below which windage does not apply
+
 vector windage_meters(particle p, field3d wind, double dt, double windage_coeff);
 
 #endif // WINDAGE
