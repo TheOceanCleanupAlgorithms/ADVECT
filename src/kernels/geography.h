56b932d099d9026b9127a1529c8acac1fd8ede61
Axel Peytavin <a.peytavin@theoceancleanup.com>
2020-11-17 07:24:51 +0100

    If position invalid, send in sahara (#53)

--- src/kernels/geography.h
+++ src/kernels/geography.h
@@ -1,6 +1,9 @@
 #ifndef GEOGRAPHY
 #define GEOGRAPHY
 
+#define INVALID_POSITION_LON 22.920583
+#define INVALID_POSITION_LAT 11.831290
+
 double degrees_lat_to_meters(double dy, double y);
 double degrees_lon_to_meters(double dx, double y);
 double meters_to_degrees_lon(double dx_meters, double y);
