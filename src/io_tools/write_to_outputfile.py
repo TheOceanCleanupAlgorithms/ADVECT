a5076fd25ad0b2467b36725f63850859a53ce1a0
Axel Peytavin <a.peytavin@theoceancleanup.com>
2020-11-17 10:24:17 +0100

    Split outputs in year (#57)
    * Split outputs in year
    
    * Feedback. Formatting just a lil' bit
    
    * Cleaning

--- src/io_tools/write_to_outputfile.py
+++ src/io_tools/write_to_outputfile.py
@@ -6,25 +6,38 @@ import numpy as np
 
 class OutputWriter:
     def __init__(self, out_path: Path):
-        self.path = out_path
-        self.created = False
+        if not out_path.is_dir():
+            out_path.mkdir(out_path)
+
+        self.folder_path = out_path
+        self.current_year = None
+        self.current_path = None
+
+    def _set_current_year(self, year: int):
+        self.current_year = year
+        self.current_path = self.folder_path.joinpath(f"parts_{year}.nc")
 
     def write_output_chunk(self, chunk: xr.Dataset):
-        if not self.created:
-            self._write_first_chunk(chunk)
-            self.created = True
-        else:
-            self._append_chunk(chunk)
+        beginning_year = chunk.time.dt[0].year
+        end_year = chunk.time.dt[-1].year
+
+        for year in range(beginning_year, end_year + 1):
+            chunk_year = chunk.isel({"time": chunk.time.dt.year == year})
+            if year != self.current_year:
+                self._set_current_year(year)
+                self._write_first_chunk(chunk_year)
+            else:
+                self._append_chunk(chunk_year)
 
     def _write_first_chunk(self, chunk: xr.Dataset):
-        with nc.Dataset(self.path, mode="w") as ds:
+        with nc.Dataset(self.current_path, mode="w") as ds:
             ds.createDimension("time", None)  # unlimited dimension
             ds.createDimension("p_id", len(chunk.p_id))
 
             time = ds.createVariable("time", np.float64, ("time",))
             time.units = "seconds since 1970-01-01 00:00:00.0"
             time.calendar = "gregorian"
-            time[:] = chunk.time.values.astype('datetime64[s]').astype(np.float64)
+            time[:] = chunk.time.values.astype("datetime64[s]").astype(np.float64)
 
             p_id = ds.createVariable("p_id", chunk.p_id.dtype, ("p_id",))
             p_id[:] = chunk.p_id.values
@@ -40,16 +53,20 @@ class OutputWriter:
             release_date = ds.createVariable("release_date", np.float64, ("p_id",))
             release_date.units = "seconds since 1970-01-01 00:00:00.0"
             release_date.calendar = "gregorian"
-            release_date[:] = chunk.release_date.values.astype('datetime64[s]').astype(np.float64)
+            release_date[:] = chunk.release_date.values.astype("datetime64[s]").astype(
+                np.float64
+            )
 
     def _append_chunk(self, chunk: xr.Dataset):
-        with nc.Dataset(self.path, mode="a") as ds:
-            time = ds.variables['time']
+        with nc.Dataset(self.current_path, mode="a") as ds:
+            time = ds.variables["time"]
             start_t = len(time)
-            time[start_t:] = chunk.time.values.astype('datetime64[s]').astype(np.float64)
+            time[start_t:] = chunk.time.values.astype("datetime64[s]").astype(
+                np.float64
+            )
 
-            lon = ds.variables['lon']
+            lon = ds.variables["lon"]
             lon[:, start_t:] = chunk.lon.values
 
-            lat = ds.variables['lat']
+            lat = ds.variables["lat"]
             lat[:, start_t:] = chunk.lat.values
