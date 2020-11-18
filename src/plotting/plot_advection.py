71ecbad5051f78bac8d020562a8b3c4fa13db031
dklink <dougdklink3@gmail.com>
2020-11-18 11:14:23 -1000

    added animation saving

--- src/plotting/plot_advection.py
+++ src/plotting/plot_advection.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,28 +7,9 @@ import cartopy.crs as ccrs
 import xarray as xr
 import matplotlib.cm as cm
 import matplotlib.colors as mcol
-
-
-def plot_advection(P, time, field, streamfunc=True, ax=None):
-    # plot le advection
-    if ax is None:
-        _, ax = plt.subplots(1, 1)
-    t_idx = -1
-    dot, = ax.plot(P[:, 0, 0], P[:, 0, 1], '.', markersize=5)
-
-    for i in range(len(time)):
-        new_t_idx = np.argmin(np.abs(field.time - time[i]))
-        if new_t_idx != t_idx and streamfunc:
-            t_idx = new_t_idx
-            ax.clear()
-            dot, = ax.plot(P[:, i, 0], P[:, i, 1], '.', markersize=5)
-            ax.streamplot(field.x, field.y, field.U[t_idx].T, field.V[t_idx].T)
-        dot.set_xdata(P[:, i, 0])
-        dot.set_ydata(P[:, i, 1])
-        ax.set_title('t={:.2f}'.format(time[i]))
-        ax.set_xlim([min(field.x), max(field.x)])
-        ax.set_ylim([min(field.y), max(field.y)])
-        plt.pause(.01)
+import matplotlib.animation as manimation
+from tqdm import tqdm
+import subprocess
 
 
 def plot_ocean_trajectories(outputfile_path: str):
@@ -38,9 +21,10 @@ def plot_ocean_trajectories(outputfile_path: str):
     ax.coastlines()
 
     ax.plot(P.lon.transpose('time', 'p_id'), P.lat.transpose('time', 'p_id'), '.')
+    plt.show()
 
 
-def plot_ocean_advection(outputfile_path: str, lon_range=(-180, 180), lat_range=(-90, 90)):
+def animate_ocean_advection(outputfile_path: str, lon_range=(-180, 180), lat_range=(-90, 90), save: bool=False):
     P = xr.open_dataset(outputfile_path)
     # plot le advection
     proj = ccrs.PlateCarree()
@@ -58,18 +42,50 @@ def plot_ocean_advection(outputfile_path: str, lon_range=(-180, 180), lat_range=
                      s=5, norm=mcol.Normalize(vmin=P.depth.min(), vmax=P.depth.max()))
     cbar = plt.colorbar(mappable=dot, ax=ax)
     cbar.ax.set_ylabel('Depth (m)')
+
+    if save:
+        animate_ocean_advection_to_disk(outputfile_path, P, fig, ax, dot)
+    else:
+        animate_ocean_advection_live(P, ax, dot)
+
+
+def animate_ocean_advection_live(P, ax, dot):
     for i in range(len(P.time)):
         dot.set_offsets(np.c_[np.array([P.isel(time=i).lon, P.isel(time=i).lat]).T])
         dot.set_array(P.isel(time=i).depth.values)
         ax.set_title(P.time.values[i])
         ax.set_ylim(-90, 90)
         plt.pause(.005)
+    plt.show()
+
+
+def animate_ocean_advection_to_disk(outputfile_path, P, fig, ax, dot):
+    FFMpegWriter = manimation.writers['ffmpeg']
+    writer = FFMpegWriter(fps=30)
+    outfile = Path(outputfile_path).with_suffix('.mp4')
+    print("Creating Movie...")
+    with writer.saving(fig, outfile=outfile, dpi=150):
+        for i in tqdm(range(len(P.time))):
+            dot.set_offsets(np.c_[np.array([P.isel(time=i).lon, P.isel(time=i).lat]).T])
+            dot.set_array(P.isel(time=i).depth.values)
+            ax.set_title(P.time.values[i])
+            ax.set_ylim(-90, 90)
+            writer.grab_frame()
+
+    plt.close()
+    print("Opening Movie...")
+    subprocess.call(['open', outfile])  # this won't work except on mac.
 
 
 @click.command()
 @click.argument("outputfile_path", type=click.Path(exists=True, dir_okay=False, readable=True))
-def plot_ocean_advection_CLI(outputfile_path: str):
-    plot_ocean_advection(outputfile_path)
+@click.option("-s", "--save_to_disk", is_flag=True)
+@click.option("-t", "--trajectories", is_flag=True)
+def plot_ocean_advection_CLI(outputfile_path: str, save_to_disk: bool, trajectories: bool):
+    if trajectories:
+        plot_ocean_trajectories(outputfile_path)
+    else:
+        animate_ocean_advection(outputfile_path, save=save_to_disk)
 
 
 if __name__ == '__main__':
