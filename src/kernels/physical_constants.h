e2771970ff22f7a40bb3b851722d4da4a5a39851
dklink <dougdklink3@gmail.com>
2020-11-27 12:42:37 -1000

    documented constants

--- src/kernels/physical_constants.h
+++ src/kernels/physical_constants.h
@@ -2,7 +2,8 @@
 #define PHYSICAL_CONSTANTS
 
 #define ACC_GRAVITY -9.81  // m s^-2
-#define DENSITY_SEAWATER  1025  // kg m^-3
-#define KINEMATIC_VISCOSITY_SEAWATER 0.0000015 // m^2 s^-1
+#define DENSITY_SEAWATER  1025  // kg m^-3, typical value for surface density (no citation, this is well known)
+#define KINEMATIC_VISCOSITY_SEAWATER 0.00000118// m^2 s^-1, typical value (ITTC fresh water and seawater properties,
+                                            // https://ittc.info/media/4048/75-02-01-03.pdf)
 
 #endif // PHYSICAL_CONSTANTS
