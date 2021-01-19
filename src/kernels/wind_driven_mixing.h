1e56c87746f4d0c80d62bb0d6d2f561ac870c1fd
dklink <dougdklink3@gmail.com>
2021-01-18 14:49:40 -1000

    comment

--- src/kernels/wind_driven_mixing.h
+++ src/kernels/wind_driven_mixing.h
@@ -6,7 +6,7 @@
 #include "fields.h"
 #include "random.h"
 
-#define MLD_in_terms_of_wave_height -10
+#define MLD_in_terms_of_wave_height -10  // reasonable approximation, see D'Asaro et al 2013 Figure 1 for evidence
 
 double sample_concentration_profile(double wind_speed_10m, double rise_velocity, random_state *rstate);
 double mixed_layer_depth(double wind_speed_10m);
