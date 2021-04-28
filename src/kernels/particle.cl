TheOceanCleanupAlgorithms/ADVECT

commit 354ed4c8bc5fd16fc630b02c4772acd5087cf3d0

Fix bug for very speedy sinkers (#110)
* Added requirement for a bathymetry file associated with current dataset; added loading for this bathymetry file.

* pass bathy into kernel

* use bathymetry instead of z_floor; fix tests

* added tweak to the slippery coastlines algorithm to clip particles to bathymetry.  Some corrections re: model domain.

* rolled back bathymetry requirement; instead, calculates it automatically, to guarantee 'bathymetry' exactly matches the kernel's idea of ocean domain (i.e., everywhere that U/V/W is non-null

* fixed small bug associated with new boundary handling

* added asserts for robustness

* small efficiency improvement

* comment correction

src/kernels/particle.cl
@@ -32,6 +32,19 @@ particle update_position_no_beaching(particle p, vector displacement_meters, fie
     // displacement will move particle out of ocean (aka, onto shoreline, or into bathymetry).
     // We don't want this, so we'll modify displacement (but as little as possible)
 
+    // first, we modify the displacement's z component based on bathymetry
+    // this is essential to allow huge vertical displacements to clip to the bathymetry.
+    double bathy_at_p = find_nearest_bathymetry(p, field);
+    double bathy_at_new_p = find_nearest_bathymetry(new_p, field);
+    // we should try and push it up as little as possible; fmin determines the clipping point.
+    // then the fmax clips the z up if it's below the clipping point.
+    double clipped_z = fmax(new_p.z, fmin(bathy_at_p, bathy_at_new_p));
+    displacement_meters.z = clipped_z - p.z;
+
+    // check simple case again now that displacement has been adjusted to match bathymetry
+    new_p = update_position(p, displacement_meters);
+    if (in_ocean(new_p, field)) return new_p;
+
     // split displacement into components and sort them
     vector ordered_components[3];
     resolve_and_sort(displacement_meters, ordered_components);
@@ -91,17 +104,21 @@ vector find_nearest_vector(particle p, field3d field, bool zero_nans) {
     return index_vector_field(field, find_nearest_neighbor(p, field), zero_nans);
 }
 
+double find_nearest_bathymetry(particle p, field3d field) {
+    grid_point neighbor = {
+        .x_idx = find_nearest_neighbor_idx(p.x, field.x, field.x_len, field.x_spacing),
+        .y_idx = find_nearest_neighbor_idx(p.y, field.y, field.y_len, field.y_spacing),
+    };
+    return index_bathymetry(field, neighbor);
+}
+
 vector find_nearby_non_null_vector(particle p, field3d field) {
     return double_jack_search(find_nearest_neighbor(p, field), field);
 }
 
 bool in_ocean(particle p, field3d field) {
-    /* where'er you find the vector nan to be,
+    /* where'er you are below bathymetry,
        you sure as heck can bet this ain't the sea.
         -- William Shakespeare */
-    grid_point gp = find_nearest_neighbor(p, field);
-    vector V = index_vector_field(field, gp, false);
-    bool position_is_nan = (isnan(V.x) || isnan(V.y) || isnan(V.z));
-    bool too_deep = p.z < field.z_floor;
-    return !(position_is_nan || too_deep);
+    return p.z >= find_nearest_bathymetry(p, field);
 }

src/drivers/create_bathymetry.py
@@ -1,0 +1,37 @@
+import xarray as xr
+import numpy as np
+
+
+def create_bathymetry(currents: xr.Dataset) -> xr.Dataset:
+    """Creates bathymetry for a current dataset, which encodes its ocean domain.
+        Method: identifies the lower depth bound of the shallowest
+        ocean cell (non-null) in each vertical grid column."""
+    assert np.all(currents.depth <= 0), "depth coordinate must be positive up"
+    assert np.all(np.diff(currents.depth) > 0), "depth coordinate must be sorted ascending"
+
+    # In the kernel, particles look up data based on the nearest cell-center.
+    # Thus cell bounds are the midpoints between each centers.
+    # Very top cell bound is surface, and bottom cell bounds are
+    # assumed to be symmetric about bottom cell center.
+    depth_diff = np.diff(currents.depth)
+    depth_bnds = np.concatenate([
+        currents.depth.values[:1] - depth_diff[0] / 2,
+        currents.depth.values[:-1] + depth_diff/2,
+        [0],
+    ])
+
+    first_timestep = currents.isel(time=0)  # only need one timestep
+    land = first_timestep.U.isnull() | first_timestep.V.isnull() | first_timestep.W.isnull()
+
+    bathy = (
+        (~land)
+        .assign_coords({"depth": depth_bnds[:-1]})
+        .idxmax(dim="depth")
+        .where(~land.isel(depth=-1), depth_bnds[-1])
+    )
+
+    bathy = bathy.drop(["time", "depth"])
+    bathy.name = "bathymetry"
+    bathy.attrs = {"units": "m", "positive": "up"}
+
+    return bathy

src/drivers/opencl_driver_3D.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from typing import Tuple, Optional, List
 from pathlib import Path
 from drivers.advection_chunking import chunk_advection_params
+from drivers.create_bathymetry import create_bathymetry
 from io_tools.OutputWriter import OutputWriter
 from kernel_wrappers.Kernel3D import Kernel3D, AdvectionScheme
 from kernel_wrappers.kernel_constants import EXIT_CODES
@@ -48,7 +49,6 @@ def openCL_advect(
     :param save_every: how many timesteps between saving state.  Must divide num_timesteps.
     :param advection_scheme: scheme to use, listed in the AdvectionScheme enum
     :param eddy_diffusivity: xarray Dataset storing vertical profiles of eddy diffusivities
-    :param density_profile: xarray Dataset storing vertical profile of seawater density
     :param max_wave_height: caps parameterization in kernel; see config_specifications.md
     :param wave_mixing_depth_factor: scales depth of mixing in kernel; see config_specifications.md
     :param windage_multiplier: multiplies the default windage, which is based on emerged area
@@ -67,6 +67,10 @@ def openCL_advect(
         context = cl.create_some_context(interactive=True)
     else:
         context = cl.create_some_context(answers=list(platform_and_device))
+
+    # encode the model domain, taken as where all the current components are non-null, as bathymetry
+    print("Calculating bathymetry of current dataset...")
+    current = xr.merge((current, create_bathymetry(current)))
 
     # get the minimum RAM available on the specified compute devices.
     print("Chunking Datasets...")

src/kernel_wrappers/Kernel3D.py
@@ -69,6 +69,7 @@ class Kernel3D:
             self.current_U = current.U.values.astype(np.float32, copy=False).ravel()  # astype will still copy if variable is not already float32
             self.current_V = current.V.values.astype(np.float32, copy=False).ravel()
             self.current_W = current.W.values.astype(np.float32, copy=False).ravel()
+            self.current_bathy = current.bathymetry.values.astype(np.float32, copy=False).ravel()
         # wind vector field
         print("\t\tLoading Wind Data...")
         if windage_multiplier is not None:
@@ -141,7 +142,7 @@ class Kernel3D:
         print("\t\tWriting buffers to compute device...")
         write_start = time.time()
         d_current_x, d_current_y, d_current_z, d_current_t,\
-            d_current_U, d_current_V, d_current_W,\
+            d_current_U, d_current_V, d_current_W, d_current_bathy,\
             d_wind_x, d_wind_y, d_wind_z, d_wind_t, d_wind_U, d_wind_V, \
             d_seawater_density_x, d_seawater_density_y, d_seawater_density_z, d_seawater_density_t, \
             d_seawater_density_values, \
@@ -151,7 +152,7 @@ class Kernel3D:
             (cl.Buffer(self.context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=hostbuf)
              for hostbuf in
              (self.current_x, self.current_y, self.current_z, self.current_t,
-              self.current_U, self.current_V, self.current_W,
+              self.current_U, self.current_V, self.current_W, self.current_bathy,
               self.wind_x, self.wind_y, self.wind_t, self.wind_z, self.wind_U, self.wind_V,
               self.seawater_density_x, self.seawater_density_y, self.seawater_density_z, self.seawater_density_t,
               self.seawater_density_values,
@@ -174,7 +175,7 @@ class Kernel3D:
             d_current_y, np.uint32(len(self.current_y)),
             d_current_z, np.uint32(len(self.current_z)),
             d_current_t, np.uint32(len(self.current_t)),
-            d_current_U, d_current_V, d_current_W,
+            d_current_U, d_current_V, d_current_W, d_current_bathy,
             d_wind_x, np.uint32(len(self.wind_x)),
             d_wind_y, np.uint32(len(self.wind_y)),
             d_wind_z,

src/kernels/fields.cl
@@ -51,23 +51,15 @@ vector index_vector_field(field3d field, grid_point gp, bool zero_nans) {
     return V;
 }
 
+double index_bathymetry(field3d field, grid_point gp) {
+    /* assumption: gp.[dim]_idx args will be in [0, field.[dim]_len - 1] */
+    size_t flat_index = gp.y_idx*field.x_len + gp.x_idx;
+    return field.bathy ? field.bathy[flat_index] : NAN;  // ternary expression stops indexing into an undefined variable
+}
+
 double calculate_spacing(__global const double *arr, const unsigned int arr_len) {
     if (arr_len > 1) {
         return (arr[arr_len-1] - arr[0]) / (arr_len - 1);
-    } else {
-        return NAN;
-    }
-}
-
-double calculate_coordinate_floor(__global const double *arr, const unsigned int arr_len) {
-    /*
-     * Calculates the lower boundary of a coordinate array, i.e. the edge of the lowest grid cell.
-     * arr references the centers of grid cells, hence the extrapolation
-    */
-    if (arr_len > 1) {
-        return arr[0] - (arr[1] - arr[0]) / 2;  // linear extrapolation
-    } else if (arr_len == 1) {
-        return arr[0];
     } else {
         return NAN;
     }
@@ -91,7 +83,6 @@ bool field_element_is_null(field3d field, grid_point gp) {
         (isnan(v.z) && field.W != 0)
     );
 }
-
 
 vector double_jack_search(grid_point gp, field3d field) {
     /* This function returns some non-null vector from "field" which is nearby grid cell "gp".

src/kernels/fields.h
@@ -7,8 +7,7 @@ typedef struct field3d {
     __global const double *x, *y, *z, *t;
     const unsigned int x_len, y_len, z_len, t_len;
     const double x_spacing, y_spacing, t_spacing;
-    __global const float *U, *V, *W;
-    const double z_floor;  // bottom edge of lowest layer
+    __global const float *U, *V, *W, *bathy;
     bool x_is_circular;  // explicitly notates whether x is a circular array (e.g. full global dataset vs regional)
 } field3d;
 
@@ -20,10 +19,10 @@ typedef struct grid_point {
 } grid_point;
 
 vector index_vector_field(field3d field, grid_point gp, bool zero_nans);
+double index_bathymetry(field3d field, grid_point gp);
 unsigned int find_nearest_neighbor_idx(double value, __global const double *arr, const unsigned int arr_len, const double spacing);
 unsigned int find_nearest_neighbor_idx_non_uniform(double value, __global const double *arr, const unsigned int arr_len);
 double calculate_spacing(__global const double *arr, const unsigned int arr_len);
-double calculate_coordinate_floor(__global const double *arr, const unsigned int arr_len);
 bool x_is_circular(field3d field);
 vector double_jack_search(grid_point gp, field3d field);
 

src/kernels/kernel_3d.cl
@@ -32,6 +32,7 @@ __kernel void advect(
     __global const float *current_U,        // m / s, shape=(t, z, y, x) flattened, 32 bit to save space
     __global const float *current_V,        // m / s
     __global const float *current_W,        // m / s
+    __global const float *current_bathy,    // m, positive up, shape=(y, x) flattened
     /* 10-meter wind vector field */
     __global const double *wind_x,          // lon, Deg E (-180 to 180), uniform spacing
     const unsigned int wind_x_len,          // 1 <= wind_x_len <= UINT_MAX + 1
@@ -97,7 +98,7 @@ __kernel void advect(
                      .y_spacing = calculate_spacing(current_y, current_y_len),
                      .t_spacing = calculate_spacing(current_t, current_t_len),
                      .U = current_U, .V = current_V, .W = current_W,
-                     .z_floor = calculate_coordinate_floor(current_z, current_z_len)};  // bottom edge of lowest layer
+                     .bathy = current_bathy};
     current.x_is_circular = x_is_circular(current);
 
     // turn 2d wind into 3d wind with singleton z
@@ -106,8 +107,7 @@ __kernel void advect(
                     .x_spacing = calculate_spacing(wind_x, wind_x_len),
                     .y_spacing = calculate_spacing(wind_y, wind_y_len),
                     .t_spacing = calculate_spacing(wind_t, wind_t_len),
-                    .U = wind_U, .V = wind_V, .W = 0,
-                    .z_floor = 0};
+                    .U = wind_U, .V = wind_V, .W = 0, .bathy = 0};
     wind.x_is_circular = x_is_circular(wind);
 
     field3d seawater_density = {
@@ -116,8 +116,7 @@ __kernel void advect(
         .x_spacing = calculate_spacing(seawater_density_x, seawater_density_x_len),
         .y_spacing = calculate_spacing(seawater_density_y, seawater_density_y_len),
         .t_spacing = NAN,
-        .U = seawater_density_values, .V = 0, .W = 0,
-        .z_floor = calculate_coordinate_floor(seawater_density_z, seawater_density_z_len),  // bottom edge of lowest layer
+        .U = seawater_density_values, .V = 0, .W = 0, .bathy = 0,
     };
     seawater_density.x_is_circular = x_is_circular(seawater_density);
 

src/kernels/particle.h
@@ -21,6 +21,7 @@ void write_p(particle p, __global float *X_out, __global float *Y_out, __global 
 bool in_ocean(particle p, field3d field);
 grid_point find_nearest_neighbor(particle p, field3d field);
 vector find_nearest_vector(particle p, field3d field, bool zero_nans);
+double find_nearest_bathymetry(particle p, field3d field);
 vector find_nearby_non_null_vector(particle p, field3d field);
 
 #endif // PARTICLE

commit cc88625ba590929656de55b07cd5cfda28cac81e

fixed bug introduced by PR #110

src/kernels/particle.cl
@@ -120,5 +120,6 @@ bool in_ocean(particle p, field3d field) {
     /* where'er you are below bathymetry,
        you sure as heck can bet this ain't the sea.
         -- William Shakespeare */
-    return p.z >= find_nearest_bathymetry(p, field);
+    double bathy = find_nearest_bathymetry(p, field);
+    return (bathy < 0) && (p.z >= bathy);
 }