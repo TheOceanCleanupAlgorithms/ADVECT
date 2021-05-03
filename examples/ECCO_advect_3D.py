f445a62bbe2c5868fef103add4a01077651d6b69
dklink <dougdklink3@gmail.com>
2021-05-03 11:35:45 -1000

    removed relative paths in example runners

--- examples/ECCO_advect_3D.py
+++ examples/ECCO_advect_3D.py
@@ -5,24 +5,25 @@ import sys
 from pathlib import Path
 from datetime import datetime, timedelta
 
-sys.path.append("../src")
+examples_root = Path(__file__).parent
+sys.path.append(str(examples_root.parent / "src"))
 from plotting.plot_advection import animate_ocean_advection
 from run_advector_3D import run_advector_3D
 
 WINDAGE_MULTIPLIER = 1  # multiplier of default windage formulation (based on emerged surface area)
 
-sourcefile = 'sourcefiles/3D_uniform_source_2015.nc'
+sourcefile = str(examples_root / 'sourcefiles/3D_uniform_source_2015.nc')
 if __name__ == '__main__':
     out_paths = run_advector_3D(
-        output_directory=f'outputfiles/ECCO_2015_3D/{Path(sourcefile).stem}/',
+        output_directory=str(examples_root / 'outputfiles/ECCO_2015_3D' / Path(sourcefile).stem),
         sourcefile_path=sourcefile,
-        configfile_path='configfiles/config.nc',
-        u_water_path='ECCO/ECCO_interp/U_2015*.nc',
-        v_water_path='ECCO/ECCO_interp/V_2015*.nc',
-        w_water_path='ECCO/ECCO_interp/W_2015*.nc',
-        u_wind_path='ncep_ncar_doe_ii/uwnd.10m.gauss.2015.nc',
-        v_wind_path='ncep_ncar_doe_ii/vwnd.10m.gauss.2015.nc',
-        seawater_density_path='ECCO/ECCO_interp/RHO_2015.nc',
+        configfile_path=str(examples_root / 'configfiles/config.nc'),
+        u_water_path=str(examples_root / 'ECCO/ECCO_interp/U_2015*.nc'),
+        v_water_path=str(examples_root / 'ECCO/ECCO_interp/V_2015*.nc'),
+        w_water_path=str(examples_root / 'ECCO/ECCO_interp/W_2015*.nc'),
+        u_wind_path=str(examples_root / 'ncep_ncar_doe_ii/uwnd.10m.gauss.2015.nc'),
+        v_wind_path=str(examples_root / 'ncep_ncar_doe_ii/vwnd.10m.gauss.2015.nc'),
+        seawater_density_path=str(examples_root / 'ECCO/ECCO_interp/RHO_2015.nc'),
         wind_varname_map={'uwnd': 'U', 'vwnd': 'V', 'level': 'depth'},
         advection_start_date=datetime(year=2015, month=1, day=1, hour=12),
         timestep=timedelta(hours=1),
