9f59fcf078dbf11b228f07c3614d0d03cf6ad30a
dklink <dougdklink3@gmail.com>
2021-06-02 10:57:34 -1000

    cleaned up wind downloading, updated paths in example runners

--- examples/ECCO_advect_2D.py
+++ examples/ECCO_advect_2D.py
@@ -18,8 +18,8 @@ from plotting.plot_advection import animate_ocean_advection, plot_ocean_trajecto
 from datetime import datetime, timedelta
 
 
-ECCO_U_PATH = examples_root / "ECCO/ECCO_interp/U_2015*.nc"
-ECCO_V_PATH = examples_root / "ECCO/ECCO_interp/V_2015*.nc"
+ECCO_U_PATH = examples_root / "ECCO/currents/U_2015*.nc"
+ECCO_V_PATH = examples_root / "ECCO/currents/V_2015*.nc"
 
 if __name__ == "__main__":
     sourcefile_path = examples_root / "sourcefiles/2D_uniform_source_2015.nc"
@@ -31,12 +31,14 @@ if __name__ == "__main__":
     ADVECTION_START = datetime(2015, 1, 1)
     ADVECTION_END = datetime(2016, 1, 1)
     out_paths = run_advector_2D(
-        output_directory=f"outputfiles/ECCO_2015_2D/{sourcefile_path.stem}",
+        output_directory=str(
+            examples_root / f"outputfiles/ECCO_2015_2D/{sourcefile_path.stem}"
+        ),
         sourcefile_path=str(sourcefile_path),
         u_water_path=str(ECCO_U_PATH),
         v_water_path=str(ECCO_V_PATH),
-        u_wind_path="ncep_ncar_doe_ii/uwnd.10m.gauss.2015.nc",  # comment out these
-        v_wind_path="ncep_ncar_doe_ii/vwnd.10m.gauss.2015.nc",  # lines to disable
+        u_wind_path=str(examples_root / "ncep_ncar_doe_ii/uwnd.10m.gauss.2015.nc"),
+        v_wind_path=str(examples_root / "ncep_ncar_doe_ii/vwnd.10m.gauss.2015.nc"),
         wind_varname_map={"uwnd": "U", "vwnd": "V", "level": "depth"},  # wind
         windage_coeff=0.005,
         eddy_diffusivity=200,  # m^2 / s
